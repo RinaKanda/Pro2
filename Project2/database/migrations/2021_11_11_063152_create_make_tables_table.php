89110b79df696cfc537d35a0c6a15cbc7846a048
Kanda <st072022@m01.kyoto-kcg.ac.jp>
2021-11-20 23:03:21 +0900

    dbset2

---
+++ Project2/database/migrations/2021_11_11_063152_create_make_tables_table.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMakeTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clinics', function (Blueprint $table) {
+            $table->integer('clinic_id')->primary();
+            $table->string('clinic_name');
+            $table->string('address');
+        });
+
+        Schema::create('vaccination_datas', function (Blueprint $table) {
+            $table->integer('vaccination_data_id')->primary();
+            $table->integer('clinic_id');
+            $table->date('vaccination_date');
+            $table->time('vaccination_time');
+            $table->integer('capacity');
+            $table->integer('reserve_counts');
+
+            $table->foreign('clinic_id')->references('clinic_id')->on('clinics');
+        });
+
+        Schema::create('reserve_people', function (Blueprint $table) {
+            $table->integer('Reserve_person_id')->primary();
+            $table->string('tickets_number');
+            $table->string('birthday');
+        });
+
+        Schema::create('reserves', function (Blueprint $table) {
+            $table->integer('reserve_id')->primary();
+            $table->integer('reserve_person_id');
+            $table->integer('vaccination_data_id');
+            $table->string('email');
+            $table->time('vaccination_time');
+            $table->dateTime('created_at');
+
+            $table->foreign('vaccination_data_id')->references('vaccination_data_id')->on('vaccination_datas');
+            $table->foreign('reserve_person_id')->references('reserve_person_id')->on('reserve_people');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reserves');
+        Schema::dropIfExists('reserve_people');
+        Schema::dropIfExists('vaccination_datas');
+        Schema::dropIfExists('clinics');
+    }
+}
---
+++ Project2/database/seeders/tablesSeeder.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class tablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //DB::table('reserves')->truncate();
+        //DB::table('reserve_people')->truncate();
+        //DB::table('vaccination_datas')->truncate();
+        //DB::table('clinics')->truncate();
+
+        DB::table('reserves')->delete();
+        DB::table('reserve_people')->delete();
+        DB::table('vaccination_datas')->delete();
+        DB::table('clinics')->delete();
+
+        \DB::table('clinics')->insert([
+                ['clinic_id' => 1000,
+                'clinic_name' => 'A病院',
+                'address' => '京都府京都市左京区'],
+                ['clinic_id' => 1001,
+                'clinic_name' => 'B病院',
+                'address' => '京都府京都市左京区'],
+                ['clinic_id' => 1002,
+                'clinic_name' => 'Cクリニック',
+                'address' => '京都府京都市伏見区'],
+            ]);
+
+        \DB::table('vaccination_datas')->insert([
+                //A
+                ['vaccination_data_id' => 100,
+                'clinic_id' => 1000,
+                'vaccination_date' => '2022-03-07',
+                'vaccination_time' => '16:00',
+                'capacity' => 20,
+                'reserve_counts' => 2,
+            ],
+            ['vaccination_data_id' => 101,
+                'clinic_id' => 1000,
+                'vaccination_date' => '2022-03-07',
+                'vaccination_time' => '16:30',
+                'capacity' => 20,
+                'reserve_counts' => 20,
+            ],
+            ['vaccination_data_id' => 102,
+                'clinic_id' => 1000,
+                'vaccination_date' => '2022-03-07',
+                'vaccination_time' => '17:00',
+                'capacity' => 20,
+                'reserve_counts' => 20,
+            ],
+            ['vaccination_data_id' => 103,
+                'clinic_id' => 1000,
+                'vaccination_date' => '2022-03-07',
+                'vaccination_time' => '17:30',
+                'capacity' => 20,
+                'reserve_counts' => 18,
+            ],
+            ['vaccination_data_id' => 104,
+                'clinic_id' => 1000,
+                'vaccination_date' => '2022-03-08',
+                'vaccination_time' => '16:00',
+                'capacity' => 20,
+                'reserve_counts' => 20,
+            ],
+            ['vaccination_data_id' => 105,
+                'clinic_id' => 1000,
+                'vaccination_date' => '2022-03-08',
+                'vaccination_time' => '16:30',
+                'capacity' => 20,
+                'reserve_counts' => 20,
+            ],
+            ['vaccination_data_id' => 106,
+                'clinic_id' => 1000,
+                'vaccination_date' => '2022-03-08',
+                'vaccination_time' => '17:00',
+                'capacity' => 20,
+                'reserve_counts' => 20,
+            ],
+            ['vaccination_data_id' => 107,
+                'clinic_id' => 1000,
+                'vaccination_date' => '2022-03-09',
+                'vaccination_time' => '16:00',
+                'capacity' => 20,
+                'reserve_counts' => 20,
+            ],
+            ['vaccination_data_id' => 108,
+                'clinic_id' => 1000,
+                'vaccination_date' => '2022-03-09',
+                'vaccination_time' => '16:30',
+                'capacity' => 20,
+                'reserve_counts' => 20,
+            ],
+            ['vaccination_data_id' => 109,
+                'clinic_id' => 1000,
+                'vaccination_date' => '2022-03-09',
+                'vaccination_time' => '17:00',
+                'capacity' => 20,
+                'reserve_counts' => 17,
+            ],
+            //B
+            ['vaccination_data_id' => 110,
+                'clinic_id' => 1001,
+                'vaccination_date' => '2022-03-09',
+                'vaccination_time' => '17:00',
+                'capacity' => 20,
+                'reserve_counts' => 20,
+            ],
+            ['vaccination_data_id' => 111,
+                'clinic_id' => 1001,
+                'vaccination_date' => '2022-03-09',
+                'vaccination_time' => '17:30',
+                'capacity' => 20,
+                'reserve_counts' => 19,
+            ],
+            ['vaccination_data_id' => 112,
+                'clinic_id' => 1001,
+                'vaccination_date' => '2022-03-09',
+                'vaccination_time' => '18:00',
+                'capacity' => 20,
+                'reserve_counts' => 20,
+            ],
+            //C
+            ['vaccination_data_id' => 113,
+                'clinic_id' => 1002,
+                'vaccination_date' => '2022-03-07',
+                'vaccination_time' => '14:00',
+                'capacity' => 20,
+                'reserve_counts' => 5,
+            ],
+            ['vaccination_data_id' => 114,
+                'clinic_id' => 1002,
+                'vaccination_date' => '2022-03-07',
+                'vaccination_time' => '14:30',
+                'capacity' => 20,
+                'reserve_counts' => 10,
+            ],
+            ['vaccination_data_id' => 115,
+                'clinic_id' => 1002,
+                'vaccination_date' => '2022-03-08',
+                'vaccination_time' => '14:00',
+                'capacity' => 20,
+                'reserve_counts' => 20,
+            ],
+            ['vaccination_data_id' => 116,
+                'clinic_id' => 1002,
+                'vaccination_date' => '2022-03-08',
+                'vaccination_time' => '14:30',
+                'capacity' => 20,
+                'reserve_counts' => 18,
+            ],
+        ]);
+
+
+        /*foreach($clinics as $clinic){
+            \App\Models\Clinic::create($clinic);
+         }
+        foreach($vaccination_datas as $vaccination_data){
+            \App\Models\Vaccination_data::create($vaccination_data);
+        } */
+        
+    }
+}
