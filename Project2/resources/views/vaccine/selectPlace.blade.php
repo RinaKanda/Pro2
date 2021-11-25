afa83a438b2c4b4efc5fca96871342df8c8c8324
ss22s <tomotora910@gmail.com>
2021-11-25 12:10:09 +0900

    public file

--- Project2/resources/views/vaccine/selectPlace.blade.php
+++ Project2/resources/views/vaccine/selectPlace.blade.php
@@ -2,12 +2,19 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+<link rel="stylesheet" type="text/css" href="/css/all.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<!-- <script src="resources/js/jquery/jquey-2.1.3.js"></script> -->
 <script>
         function ok(key){
             alert("ok:key" + key);
-            
         }
+        $(function(){
+            $('#colums').click(function(){
+                $(this).addClass("selected");
+            })
+        });
     </script>
     <meta charset="UTF-8">
     <title>Document</title>
@@ -21,7 +28,7 @@
             <tr>
             <!-- <td>{{ $clinics ?? '病院が見つからないよ！' }}</td> -->
             @foreach($clinics as $key => $clinic)
-                <tr onclick="ok({{ $clinic->clinic_id }})">
+                <tr id="colums" onclick="ok({{ $clinic->clinic_id }})">
                     <td>{{ $clinic->clinic_name }}</td>
                     <td>{{ $clinic->address }}</td>
                 </tr>
