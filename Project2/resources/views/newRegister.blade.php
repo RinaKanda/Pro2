27cd1890eb193c5a878c099deb48a6b6bf9d3baa
ss22s <tomotora910@gmail.com>
2021-12-02 13:37:04 +0900

    211202

---
+++ Project2/resources/views/newRegister.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+    
+    <style>
+        body{
+            margin-right: auto;
+            margin-left: auto;
+            width: 400px;
+        }
+        .border{
+            border:solid 2px;
+            width: 160px;
+            text-align:center;
+            margin-top: 50px;
+            margin-right: auto;
+            margin-left: auto;
+        }
+    </style>
+</head>
+
+<body>
+    <title>新規登録</title>
+    <h1>新規登録</h1>
+    {{ $keyReg }}
+
+    @if (true)
+        top
+    @else
+        newReserve
+    @endif
+</body>   
