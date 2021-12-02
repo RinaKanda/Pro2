c0418dfe3e939cf49c7e7db5a64246d681a6e237
Kanda <st072022@m01.kyoto-kcg.ac.jp>
2021-12-02 14:16:44 +0900

    models_add

--- Project2/app/Models/reservation_data.php
+++ Project2/app/Models/reservation_data.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class vaccination_data extends Model
+class reservation_data extends Model
 {
     use HasFactory;
 }
