4ee5673754f45169bd564cbfb8e143ce6494eada
ss22s <tomotora910@gmail.com>
2022-01-25 01:08:05 +0900

    0125-2

---
+++ Project2/resources/views/layouts/reserlay.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+<!-- <link rel="stylesheet" type="text/css" href="/css/all.css"> -->
+<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="/css/all.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <style>
